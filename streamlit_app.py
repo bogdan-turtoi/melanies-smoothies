76fa5fcc07c91acff51ac310008bff5737b5ac74
bogdan-turtoi <turtoi.bogdan@gmail.com>
2026-08-18 14:14:07 +0300

    Display smoothie response as a dataframe
    Replace text display of smoothie response with a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,11 @@ st.write(
   **Choose the fruits** you want in your custom smoothie!
   """
 )
+
+
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 # Get the current credentials
 # session = get_active_session()
